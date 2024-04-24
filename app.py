2e8498a937399f80b9786ea1a92f6a8362965b2e
smit <smitsuvagiya111@gmail.com>
2024-04-24 12:36:24 +0530

    Just tried

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+from flask_migrate import Migrate
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 import os
@@ -24,7 +25,7 @@ def create_app(db_url=None):
     app.config[
         "OPENAPI_SWAGGER_UI_URL"
     ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    # From here you can connect database here
+    
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db") 
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
